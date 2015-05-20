cd1539900aa87b13e0d6a8215f52311a81584d53
hojene007 <yevgeniy.levin@barcelonagse.eu>
2015-05-20 22:52:41 +0200

    añadí dos archivos que hacen scraping en completo
    periodicos: El Mundo, El Pais, Expansion

--- python/periodicosRecolecta.py
+++ python/periodicosRecolecta.py
@@ -14,6 +14,7 @@ import re
 import time
 import sys
 import os
+from selenium import webdriver
 import pandas as pd
 
 os.chdir("C:\\Users\\bluecap\\Desktop\\bluecap\\project\\scrapper\\python")
@@ -36,16 +37,20 @@ def nombresLimpiador(nombresVec, terms) :
         
     return(nombresNuevos)
 
-#### Conectando a baso de datos y tirando empresas con concurso
+### Conectando a baso de datos y tirando empresas con concurso
 host = "bluecap.cubz34n4knn2.eu-central-1.rds.amazonaws.com"
 user = "bluecap"
 password = "Bluecap2005"
-dbname = "einformaDB"
+dbname = "einformaDB"#
 
 db = MySQLdb.connect(host, user, password, dbname)
 cursor = db.cursor()
 
-queryConcurso = "SELECT * FROM einformaDB.Empresas where situacion like '%Concurso%';"
+# creiando la tabla con bing cosas 3
+#cursor.execute("DROP TABLE IF EXISTS einformaDB.bingNoticias;")
+#cursor.execute(create_table)
+
+queryConcurso = "SELECT * FROM einformaDB.Empresas where situacion like '%Concurso%' and situacion like '%CONCURSO%';"
 nombres_fechas = pd.DataFrame()
 dfAll = pd.read_sql(queryConcurso, db)
 nombres_fechas[['nombre_empresa', 'situacion']] = dfAll[['nombre_empresa', 'situacion']]
@@ -57,17 +62,147 @@ fechas =[a[1:] for a in fechas]
 nombres_fechas[['fecha_concurso']] = pd.DataFrame(fechas) 
 nombres_fechas = nombres_fechas[['nom_limpios', 'fecha_concurso']]
 
-
-''' YAHOO SEARCH '''
-"YahooNews(keys, wait=0)"
+"""
+''' Bing SEARCH '''
+"BingNews(keys, wait=0)"
 '''''''''''''''''''' 
+driver = webdriver.Chrome()
+
+insertString = "INSERT INTO einformaDB.bingNoticias (empresa, fecha, autor, link, parrafo, titulo)  values (%s, %s, %s, %s, %s, %s);"
+
+def updateDBBing(empresaDict, baseString) :
+    # se hace updating MYSQL DB 
+    for nom in empresaDict:
+        if len(empresaDict[nom])>0 :
+            for articulo in empresaDict[nom] :
+                tempDF = pd.DataFrame(articulo.items())
+                print "Se actualizo empresa '%s' y articulo se llama '%s' " % (nom.encode("utf-8"), tempDF.iloc[5, 1].encode("utf-8"))
+                cursor.execute(baseString, (tempDF.iloc[0, 1].encode("utf-8"), tempDF.iloc[1, 1].encode("utf-8"), tempDF.iloc[2, 1].encode("utf-8"), tempDF.iloc[3, 1].encode("utf-8"), tempDF.iloc[4, 1].encode("utf-8"), tempDF.iloc[5, 1].encode("utf-8"))) 
+                db.commit()
+
+
+## attacando bing 
 todasEmpresas = {}
+paraActualisarDB = {} # un diccionario temporario
+noms = list(nombres_fechas['nom_limpios'])
+ind = 0
+cadaN_updateDB = 10
+N = len(noms)
+for nom in noms :
+    #if ind > 252 :
+    infoList = BingNews(key=nom.decode("utf-8"), wait=2)
+    todasEmpresas[nom.decode("utf-8")] = infoList
+    paraActualisarDB[nom.decode("utf-8")] = infoList
+    print "he terminado yendo a yahoo para sacar info sobre empresa # %s de %s " % (ind, N)
+    if ind >0 and ind%cadaN_updateDB==0 :
+        updateDBBing(paraActualisarDB, insertString)
+        paraActualisarDB = {}
+    ind +=1
+"""    
+#############################################################################
 
-for nom in list(nombres_fechas[['nom_lipios']]) :
-    infoList = YahooNews(key=nom, wait=2)
-    todasEmpresas[nom] = infoList
+#############################################################################  
+    
+############### Attacando periodicos españoles
+queryConcurso = "SELECT * FROM einformaDB.Empresas where situacion like '%Concurso%' and situacion like '%CONCURSO%';"
+dropIfthere = "DROP TABLE IF EXISTS einformaDB.PeriodicosGrandes;"
+nuevaTabla = """CREATE TABLE einformaDB.PeriodicosGrandes
+(
+fuenta text,
+empresa text,
+fecha datetime,
+autor text,
+link text, 
+parrafo text, 
+titulo text,
+texto text
+);"""
+cursor.execute(dropIfthere)
+cursor.execute(nuevaTabla)
+insertString = "INSERT INTO einformaDB.PeriodicosGrandes (fuenta, empresa, fecha, autor, link, parrafo, titulo, texto)  values (%s, %s, %s, %s, %s, %s, %s, %s);"
 
 
+def updateDBPeriodicosGrandes(fuenta, empresaDict, baseString) :
+    # se hace updating MYSQL DB 
+    for nom in empresaDict:
+        if len(empresaDict[nom])>0 :
+            for articulo in empresaDict[nom] :
+                tempDF = pd.DataFrame(articulo.items())
+                print "Se actualizo empresa '%s' y articulo se llama '%s' " % (nom.encode("utf-8"), tempDF.iloc[5, 1].encode("utf-8"))
+                cursor.execute(baseString, (fuenta, tempDF.iloc[0, 1].encode("utf-8"), tempDF.iloc[1, 1].encode("utf-8"), tempDF.iloc[2, 1].encode("utf-8"), tempDF.iloc[3, 1].encode("utf-8"), tempDF.iloc[4, 1].encode("utf-8"), tempDF.iloc[5, 1].encode("utf-8"),  tempDF.iloc[6, 1].encode("utf-8"))) 
+                db.commit()
+
+dictElPais = {}
+dictElMundo = {}
+dictExpansion = {}
+
+paraActualisarExpansion = {}
+paraActualisarElMundo = {}
+paraActualisarElPais = {} # un diccionario temporario
+
+noms = list(nombres_fechas['nom_limpios'])
+ind = 0
+cadaN_updateDB = 10
+N = len(noms)
+indexError = 0
+for nom in noms :
+    while indexError > -1 :
+        indexError +=1
+    
+        expansionTemp = ExpansionTP2(nom, wait=2)
+        print "...........Yendo a Expansion............"
+        for link in expansionTemp :
+            index = expansionTemp.index(link)
+            print ".... cogiendo noticia %s de %s .....Expansion" % (index, len(expansionTemp))
+            if index != 0 and index%100 == 0:
+                driver.quit()
+                driver = webdriver.Chrome()
+            expansionTemp[index]["texto"] = tiraExpansion(expansionTemp[index]["link"])
+            
+            
+            
+        elmundoTemp = ElMundoTP2(nom, wait=2)
+        print "...........Yendo a El Mundo............"
+        for link in elmundoTemp :
+            index = elmundoTemp.index(link)
+            print ".... cogiendo noticia %s de %s .....El Mundo" % (index, len(elmundoTemp))
+            if index != 0 and index%100 == 0:
+                driver.quit()
+                driver = webdriver.Chrome()
+            elmundoTemp[index]["texto"] = tiraElMundo(elmundoTemp[index]["link"]) # seguiendo a cada vinculo
+            
+            
+           
+        elpaisTemp = ElPaisTP2(nom, wait =2) # variables temopranias que contenen la busqueda en cada periodoco empresa se llama "nom"
+        print "...........Yendo a El Pais............"
+        for link in elpaisTemp :
+            index = elpaisTemp.index(link)
+            print ".... cogiendo noticia %s de %s .....El Pais" % (index, len(elpaisTemp))
+            if index != 0 and index%100 == 0:
+                driver.quit()
+                driver = webdriver.Chrome()
+            elpaisTemp[index]["texto"] = tiraElPais(elpaisTemp[index]["link"]) 
+        
+        
+        dictExpansion[nom.decode("utf-8")] = expansionTemp
+        dictElMundo[nom.decode("utf-8")] = elmundoTemp
+        dictElPais[nom.decode("utf-8")] = elmundoTemp
+        
+        paraActualisarExpansion[nom.decode("utf-8")] = expansionTemp
+        paraActualisarElMundo[nom.decode("utf-8")] = elmundoTemp
+        paraActualisarElPais[nom.decode("utf-8")] = elmundoTemp
+        
+        print "he terminado yendo a yahoo para sacar info sobre empresa # %s de %s " % (ind, N)
+        if ind >0 and ind%cadaN_updateDB==0 :
+            updateDBPeriodicosGrandes("expansion", paraActualisarExpansion, insertString)
+            updateDBPeriodicosGrandes("elmundo", paraActualisarElMundo, insertString)
+            updateDBPeriodicosGrandes("elpais", paraActualisarElPais, insertString)
+            paraActualisarExpansion = {}
+            paraActualisarElMundo = {}
+            paraActualisarElPais = {}
+        ind +=1
 
 
-
+    
+    
+    
---
+++ python/periodicos_class2.py
@@ -0,0 +1,358 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 09:38:52 2015
+
+@author: bluecap
+"""
+
+__autor__ = "Yevgeniy Levin, Enrich Gilabert"
+
+from selenium import webdriver
+import time
+import pymysql
+import pymysql.cursors
+import sys
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
+
+"""
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+binary = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
+driver = webdriver.Firefox(firefox_binary=binary)
+"""
+driver = webdriver.Chrome()
+
+
+#############################################################################
+"""Tirando titulos, vinculos, etc CLASS """ 
+#############################################################################
+
+class wait_for_page_load(object):
+
+    def __init__(self, browser, tag):
+        self.browser = browser
+        self.myTag = tag
+
+    def __enter__(self):
+        self.old_page = self.browser.find_element_by_tag_name(self.myTag)
+
+    def page_has_loaded(self):
+        new_page = self.browser.find_element_by_tag_name(self.myTag)
+        return new_page.id != self.old_page.id
+
+    def __exit__(self, *_):
+        wait_for(self.page_has_loaded)
+
+#############################################################################
+"""EL PAIS"""
+#############################################################################
+
+def ElPaisTP2(key, wait=0):
+    
+    # go to the google home page
+    driver.get("http://elpais.com/buscador/")
+    time.sleep(wait)
+
+    # find the element that's name attribute is q (the google search box)
+    try :
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "formulario_busquedas")))
+        form = element
+    except :
+        print "hay alguno terible en tu codigo......EL Pais"
+
+    #form = driver.find_element_by_id('formulario_busquedas')
+    inputElement = form.find_element_by_name('qt')
+    # type in the search
+    inputElement.send_keys(key)
+    # submit the form (although google automatically searches now without submitting)
+    inputElement.submit()
+    resultados = []
+    proximaPaginaBool = 0   
+    pageDoing = 1
+    
+    def cogerDatos() :
+        time.sleep(wait)
+        #Nos movemos a lo largo de todos los resultados de la búsqueda extrayendo la info 
+        for res in driver.find_elements_by_class_name('article'):
+            try :
+                noticia = {}
+                titulo = res.find_element_by_css_selector('div.noticia h2 a').text
+                link = res.find_element_by_css_selector('div.noticia h2 a').get_attribute('href')
+                fecha = res.find_element_by_class_name('fecha').text
+                fuente = res.find_element_by_css_selector('.firma_comentarios').text
+                parrafo = res.find_element_by_tag_name('p').text
+                noticia['Empresa']=key
+                noticia['titulo']=titulo
+                noticia['link'] = link
+                noticia['autor'] =fuente
+                noticia['fecha'] =fecha
+                noticia['parrafo']=parrafo
+                resultados.append(noticia)
+            except :
+                print ("el pais no love from you ......", sys.exc_info()[0])
+    
+    try:
+        cogerDatos()
+        while proximaPaginaBool == 0 :
+            try:
+                temp = driver.find_element_by_class_name("paginacion")
+                temp2 = temp.find_elements_by_tag_name("a")
+                temp3 = temp2[0].get_attribute("href")
+                
+                if temp3.encode("utf-8") != 'javascript:void(0);' :
+                    temp2[0].click()
+                    cogerDatos()
+                    pageDoing +=1 
+                    print "       .....scraping pagina no. %s......      " % pageDoing
+                else :
+                    print "no hay mas paginas con resultados"
+                    proximaPaginaBool = 1
+            except :
+                print ("no hay mas paginas con resultados", sys.exc_info()[0])
+                proximaPaginaBool = 1
+        
+        time.sleep(wait)
+        return(resultados)
+    
+    except:
+        print('Ha ocurrido un error con la busqueda de '+key)
+        print("Unexpected error:", sys.exc_info()[0])
+        time.sleep(2)
+        
+
+#############################################################################
+"""El MUNDO"""
+#############################################################################        
+        
+        
+def ElMundoTP2(key, wait=0):
+    
+    # go to the google home page
+    driver.get("http://ariadna.elmundo.es/buscador/archivo.html")
+    # find the element that's name attribute is q (the google search box)
+    #form = driver.find_element_by_id('formulario_busquedas')
+    try :
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "q")))
+        inputElement = element
+    except :
+        print "hay alguno terible en tu codigo......"
+
+        
+    # type in the search
+    inputElement.send_keys(key)
+    # submit the form (although google automatically searches now without submitting)
+    inputElement.submit()
+    resultados = []
+    proximaPaginaBool = 0   
+    pageDoing = 1
+    
+    def cogerDatos() :
+        time.sleep(wait)
+
+        #Nos movemos a lo largo de todos los resultados de la búsqueda extrayendo la info 
+        busc = driver.find_element_by_class_name('lista_resultados')
+        for res in busc.find_elements_by_css_selector('li'):
+            try :
+                noticia = {}
+                
+                titulo = res.find_element_by_tag_name('h3').find_element_by_tag_name('a').text
+                #print(titulo)
+                link = res.find_element_by_tag_name('h3').find_element_by_tag_name('a').get_attribute('href')
+                #print(link)            
+                fecha = res.find_element_by_class_name('fecha').text
+                #print(fecha)            
+                fuente = res.find_element_by_class_name('autor').text
+                #print(fuente)            
+                parrafo = res.find_elements_by_tag_name('p')[3].text
+                #print(parrafo)            
+                noticia['Empresa']=key
+                noticia['titulo']=titulo
+                noticia['link']=link
+                noticia['autor']=fuente
+                noticia['fecha']=fecha
+                noticia['parrafo']=parrafo
+                resultados.append(noticia)
+                print ".... anexo resultad con titulo:.... %s      ....." % titulo
+            except :
+                print("Potentially no results", sys.exc_info()[0])
+
+    try : 
+        cogerDatos()
+        print "tried once"
+        while proximaPaginaBool == 0 :
+            try:
+                time.sleep(wait)
+                temp = driver.find_element_by_class_name("nav_resultados") # verificando si hay mas que una pagina
+                temp2 = temp.find_elements_by_tag_name("a")
+                temp3 = temp2[-1].text
+                if temp3.encode("utf-8") == 'Siguiente \xc2\xbb' :
+                    temp2[-1].click()
+                    print "clicking"
+                else :
+                    print "no hay mas paginas con resultados"
+                    proximaPaginaBool = 1
+                    
+                cogerDatos()
+                pageDoing +=1 
+                print "       .....scraping pagina no. %s......      " % pageDoing
+            except :
+                print "no hay mas paginas con resultados"
+                proximaPaginaBool = 1
+        print proximaPaginaBool    
+        return(resultados)
+    except :
+        print('Ha ocurrido un error con la busqueda de '+key)
+        print("Unexpected error:", sys.exc_info()[0])
+        time.sleep(2)
+        
+#############################################################################
+"""EXPANSION"""
+#############################################################################
+
+def ExpansionTP2(key, wait=2) :
+        # go to the google home page
+    driver.get("http://www.expansion.com")
+    # find the element that's name attribute is q (the google search box)
+
+    try :
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "buscar")))
+        inputElement = element
+    except :
+        print "hay alguno terible en tu codigo......"
+
+
+        
+    # type in the search
+    inputElement.send_keys(key)
+    # submit the form (although google automatically searches now without submitting)
+    inputElement.submit()
+    time.sleep(wait) # <----  no se funciona sin esto
+    resultados = []
+    proximaPaginaBool = 0   
+    pageDoing = 1
+    
+    def cogerDatos() :
+        try :
+            for res in driver.find_elements_by_css_selector('.detalle_noticia_busqueda'):
+                noticia = {}
+                titulo = res.find_element_by_css_selector('h2').text
+                link = res.find_element_by_css_selector('a').get_attribute('href')
+                fecha = res.find_element_by_css_selector('span.firma').text
+                fuente = "Expansion"
+                parrafo = res.find_element_by_css_selector(*'p').text
+                noticia['Empresa']=key
+                noticia['titulo']=titulo
+                noticia['link']=link
+                noticia['fecha']=fecha
+                noticia['autor']=fuente
+                noticia['parrafo']=parrafo
+                resultados.append(noticia)
+        except :
+            print ("why you no give me no love baby.... Expansion", sys.exc_info()[0])
+        
+    try:
+        #Nos movemos a lo largo de todos los resultados de la búsqueda extrayendo la info 
+        cogerDatos()
+        while proximaPaginaBool == 0 :
+            try :
+                time.sleep(wait)
+                temp = driver.find_element_by_class_name("siguiente")
+                temp.click()
+                cogerDatos()
+                pageDoing +=1 
+                print "       .....scraping pagina no. %s......      " % pageDoing
+            except :
+                print "... no hay mas paginas ...."
+                proximaPaginaBool = 1   
+
+        
+        time.sleep(wait)
+        return(resultados)
+        
+    except:
+        print('Ha ocurrido un error con la busqueda de '+key)
+        print("Unexpected error:", sys.exc_info()[0])
+        time.sleep(2)
+        
+        
+        
+#############################################################################
+        """PROBANDO COSAS"""
+#############################################################################
+        """
+appleTry = ElPaisTP("apple", wait=4) # se nesecito a poner mas tiempo si internet esta despacio
+appleTry = ElMundoTP("immobiliaria", wait=4) # se nesecito a poner mas tiempo si internet esta despacio
+
+
+driver.get("http://ariadna.elmundo.es/buscador/archivo.html?q=immobiliaria%20&t=1&i=1&n=10&fd=0&td=0&w=70&s=1&no_acd=1")
+temp = driver.find_element_by_class_name("nav_resultados")
+temp2 = temp.find_elements_by_tag_name("a")
+temp3 = temp2[-1].text
+if temp3.encode("utf-8") == 'Siguiente \xc2\xbb' :
+    temp2[-1].click()
+    print "clicking"
+else :
+    print "done"
+temp2[-1].click()
+
+"""
+#############################################################################
+"""Tirando texto de noticias CLASS """ 
+#############################################################################
+
+    
+def tiraElPais(vinculo) :
+    try :
+        driver.get(vinculo)
+        time.sleep(2)
+        text = driver.find_elements_by_tag_name("p")
+        textL = [a.text for a in text]
+        return(" ".join(filter(None, textL)))
+    except :
+        return("no hay texto disponible")
+    
+    
+#############################################################################
+"""Tirando texto de noticias CLASS """ 
+#############################################################################
+
+def tiraElMundo(vinculo) :
+    try :
+        driver.get(vinculo)
+        time.sleep(2)
+        text = driver.find_elements_by_tag_name("p")
+        textL = [a.text for a in text]
+        return(" ".join(filter(None, textL)))
+    except :
+        return("no hay texto disponible")
+#############################################################################
+"""Tirando texto de noticias CLASS """ 
+#############################################################################
+    
+def tiraExpansion(vinculo) :
+    try :
+        driver.get(vinculo)
+        time.sleep(2)
+        text = driver.find_elements_by_tag_name("p")
+        textL = [a.text for a in text]
+        return(" ".join(filter(None, textL)))
+    except :
+        return("no hay texto disponible")
+        
+        
+#############################################################################
+        """probando cosas variadas""" 
+#############################################################################        
+"""
+driver.get("http://economia.elpais.com/economia/2012/09/01/actualidad/1346529488_347848.html")
+#tamano
+temp = driver.find_elements_by_tag_name("p")
+textL = [a.text for a in temp]
+str_list = filter(None, textL)
+"""
