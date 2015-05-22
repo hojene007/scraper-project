925ebd134bbe3591ef5ececcd1b94f52ed019f59
hojene007 <yevgeniy.levin@barcelonagse.eu>
2015-05-22 19:55:37 +0200

    encoding practice

---
+++ python/encodingPractice.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 13:01:04 2015
+
+@author: Epoch
+"""
+a = "ISLEÑA DE PESCADO"
+print "Se actualizo empresa '%s' y articulo se llama '%s' " % (a, "mas espa)
